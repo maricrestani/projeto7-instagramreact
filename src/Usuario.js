77c4bdbae00a2ec803cd9705f7880af15158c634
mcrestani <marianascrestani@gmail.com>
2022-09-19 13:19:43 -0300

    versão final

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,55 +1,41 @@
 import React from 'react';
-
+ 
 export default function Usuario() {
-
-  const [nome, setNome] = React.useState("Catana")
-  const [usuario, setUsuario] = React.useState("catanacomics")
-  const [imagem, setImagem] = React.useState("img/catanacomics.svg")
-
-  function mudaUsuario() {
-    const nomePrompt = prompt("Insira seu nome")
-    setNome(nomePrompt)
-    const usuarioPrompt = prompt("Insira seu usuário")
-    setUsuario(usuarioPrompt)
-  }
-
-  function mudaImagem() {
-    const imagemPrompt = prompt("Insira seu link da foto de perfil")
-    setImagem(imagemPrompt)
-  }
-
-  return (
-    <div class="usuario">
-      <img onClick={mudaImagem} src={imagem} alt='' />
-      <div class="texto">
-        <strong>{usuario}</strong>
-        <span>
-          {nome}
-          <ion-icon name="pencil" onClick={mudaUsuario} ></ion-icon>
-        </span>
-      </div>
-    </div>
-  )
-}
-
-/*
-function User(props) {
-
-  return (
-    <div class="usuario">
-      <img onclick={props.clickImagem} src={props.imagem} alt=''/>
-      <div class="texto">
-        <strong>{props.usuario}</strong>
-        <span>
-          {props.nome}
-          <ion-icon name="pencil" onclick={props.clickLapis}></ion-icon>
-        </span>
-      </div>
-    </div>
-  )
-}
-
-return (
-   <User imagem="img/catanacomics.svg" usuario="catanacomics" nome="Catana" />
+ 
+ const [nome, setNome] = React.useState("Catana")
+ const [usuario, setUsuario] = React.useState("catanacomics")
+ const [imagem, setImagem] = React.useState("img/catanacomics.svg")
+ 
+ function mudaUsuario() {
+   let nomePrompt = prompt("Insira seu nome")
+   let usuarioPrompt = prompt("Insira seu usuário")
+ 
+   if (nomePrompt === "" || nomePrompt === null || usuarioPrompt === "" || usuarioPrompt === null) {}
+   else {
+     setNome(nomePrompt)
+     setUsuario(usuarioPrompt)
+   }
+ }
+ 
+ function mudaImagem() {
+   let imagemPrompt = prompt("Insira seu link da foto de perfil")
+ 
+   if (imagemPrompt === "" || imagemPrompt === null ){}
+   else {
+   setImagem(imagemPrompt)
+   }
+ }
+ 
+ return (
+   <div class="usuario">
+     <img onClick={mudaImagem} src={imagem} alt='' />
+     <div class="texto">
+       <strong>{usuario}</strong>
+       <span>
+         {nome}
+         <ion-icon name="pencil" onClick={mudaUsuario} ></ion-icon>
+       </span>
+     </div>
+   </div>
  )
-*/
+}
